4d5d9a6b2928b979d0675b6a6285413cf8051af1
Adi <Adi@roopas-mbp.lan>
2022-06-13 22:37:41 -0700

    initial commit

---
+++ Instasync/InstasyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstasyncApp.swift
+//  Instasync
+//
+//  Created by Adi on 6/8/22.
+//
+
+import SwiftUI
+
+@main
+struct InstasyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
